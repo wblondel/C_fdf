73ccaa56f07b10982ee3cf7861a0667249cf8818
William Blondel <contact@williamblondel.fr>
2017-12-08 06:17:00 +0100

    Added hello world program

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/12/08 05:55:32 by wblondel     #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/08 06:00:40 by wblondel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int		main(void)
+{
+	t_data data;
+
+	if ((data.mlx_ptr = mlx_init()) == NULL)
+		return (EXIT_FAILURE);
+	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hel")) == NULL)
+		return (EXIT_FAILURE);
+	mlx_loop(data.mlx_ptr);
+	return (EXIT_SUCCESS);
+}
---
+++ main.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.h                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/12/08 05:54:33 by wblondel     #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/08 06:00:35 by wblondel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "mlx.h"
+
+typedef struct		s_data
+{
+	void			*mlx_ptr;
+	void			*mlx_win;
+}					t_data;
