181c10a21252ba2b0edd4e537f6c4f286359438b
William Blondel <contact@williamblondel.fr>
2017-12-08 08:41:34 +0100

    example main with put_image

--- inc/main.h
+++ inc/main.h
@@ -6,7 +6,7 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/08 05:54:33 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/08 07:43:39 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/08 08:39:12 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,13 +15,27 @@
 
 # define MAIN_H
 
+# include <stdio.h>
+# include <errno.h>
+# include <math.h>
+
 # include "mlx.h"
 # include "libft.h"
 
-typedef struct		s_data
+typedef struct		s_img
+{
+	void			*img_ptr;
+	int				*data;
+	int				size_l;
+	int				bpp;
+	int				endian;
+}					t_img;
+
+typedef struct		s_mlx
 {
 	void			*mlx_ptr;
-	void			*mlx_win;
-}					t_data;
+	void			*win;
+	t_img			img;
+}					t_mlx;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/08 05:55:32 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/08 06:00:40 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/08 08:39:37 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,12 +15,49 @@
 
 int		main(void)
 {
-	t_data data;
+	int		win_width;
+	int		win_height;
+	t_mlx	mlx;
 
-	if ((data.mlx_ptr = mlx_init()) == NULL)
+	win_width = 800;
+	win_height = 600;
+
+	/*
+	** First you need to call mlx_init and store its return value.
+	*/
+	if ((mlx.mlx_ptr = mlx_init()) == NULL)
 		return (EXIT_FAILURE);
-	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hel")) == NULL)
+
+	/*
+	** Now do the same with mlx_new_window
+	*/
+	if ((mlx.win = mlx_new_window(mlx.mlx_ptr, win_width, win_height, "a")) == NULL)
 		return (EXIT_FAILURE);
-	mlx_loop(data.mlx_ptr);
+
+	/*
+	** One more time with mlx_new_image
+	*/
+	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, win_width, win_height);
+
+	/*
+	** Now the important part :
+	** mlx_get_data_addr will return a char * that is 4 time the width * height of your imqge.
+	** Why so ? Let me explain : This char * will represent your image, pixel by pixel,
+	** and the values of this array are the colors. That's why the array is 4 times bigger :
+	** you need 4 char to code the color of each pixels (one for Red, Green and Blue) and one for the alpha.
+	** But... it's not very convenient, right ? So here is my little trick : you cast
+	** mlx_get_data_addr as an int * and store it in an int *.
+	** This way, the array will have the exact same size as your window, and each index
+	** will represent one complete color of a pixel !
+	*/
+	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp,
+											&mlx.img.size_l, &mlx.img.endian);
+
+	/*
+	** Now you just have to print the image using mlx_put_image_to_window !
+	*/
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+
+	mlx_loop(mlx.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
