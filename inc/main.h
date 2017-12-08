36fd3846454ef5a60ba6a282abd4431d2382339f
William Blondel <contact@williamblondel.fr>
2017-12-08 07:09:07 +0100

    restructured files

--- inc/main.h
+++ inc/main.h
@@ -6,18 +6,24 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/08 05:54:33 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/08 06:00:35 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/08 06:57:18 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "mlx.h"
+#ifndef MAIN_H
+
+# define MAIN_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "mlx.h"
 
 typedef struct		s_data
 {
 	void			*mlx_ptr;
 	void			*mlx_win;
 }					t_data;
+
+#endif
