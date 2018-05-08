c481d962a745c947309852adca8fe82887efaf58
William Blondel <contact@williamblondel.fr>
2018-05-08 18:26:00 +0200

    Re-added debug functions

--- src/hooks_keyboard.c
+++ src/hooks_keyboard.c
@@ -6,7 +6,7 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/20 16:31:12 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/19 21:17:51 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/05/08 17:18:44 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,36 +18,24 @@ int		key_press_hook(int keycode, t_global *g)
 	key_toggle(&g->key, keycode, 1);
 	if (g->key.esc)
 		exit_hook(keycode, g);
-	/*if (g->key.d)
-		cam_set_margin(&g->cam, g->cam.margin_x -= 15, g->cam.margin_y += 15);
-	if (g->key.a)
-		cam_set_margin(&g->cam, g->cam.margin_x += 15, g->cam.margin_y -= 15);
-	if (g->key.w)
-		cam_set_margin(&g->cam, g->cam.margin_x += 15, g->cam.margin_y += 15);
-	if (g->key.s)
-		cam_set_margin(&g->cam, g->cam.margin_x -= 15, g->cam.margin_y -= 15);*/
-
-	if (g->key.d)
-		cam_set_margin(&g->cam, g->cam.margin_x -= 15, g->cam.margin_y);
 	if (g->key.a)
 		cam_set_margin(&g->cam, g->cam.margin_x += 15, g->cam.margin_y);
 	if (g->key.w)
 		cam_set_margin(&g->cam, g->cam.margin_x, g->cam.margin_y += 15);
 	if (g->key.s)
 		cam_set_margin(&g->cam, g->cam.margin_x, g->cam.margin_y -= 15);
-
+	if (g->key.d)
+		cam_set_margin(&g->cam, g->cam.margin_x -= 15, g->cam.margin_y);
 	if (g->key.plus)
 		cam_set_height_multiplier(&g->cam, g->cam.height_multiplier + 1);
 	if (g->key.minus)
 		cam_set_height_multiplier(&g->cam, g->cam.height_multiplier - 1);
-
 	if (g->key.r)
 		cam_reset(&g->cam);
-
-	/*if (g->key.f)
+	if (g->key.m)
 		print_map(&g->map);
 	if (g->key.p)
-		print_points(&g->map);*/
+		print_points(&g->map);
 	return (0);
 }
 
