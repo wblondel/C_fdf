488c24962758af546de4a450e3dc170fb73a817e
William Blondel <contact@williamblondel.fr>
2018-05-08 18:25:03 +0200

    Fixed: Crash when file contains lines with only tabs

--- src/import.c
+++ src/import.c
@@ -6,7 +6,7 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/09 13:52:26 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/25 16:24:36 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/05/08 18:19:12 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -35,10 +35,9 @@ static int		open_file(char const *filename)
 
 /*
 ** We read the file that is behind the file descriptor fd.
-** TODO: ft_strsplit() is very slow. It needs optimizations.
 */
 
-static int		read_file(int const fd, t_map *map)
+static void		read_file(int const fd, t_map *map)
 {
 	int		yx[2];
 	char	**split;
@@ -53,7 +52,8 @@ static int		read_file(int const fd, t_map *map)
 		ft_strdel(&line);
 		while (split[yx[1]])
 			yx[1]++;
-		map->width = (map->width == 0) ? yx[1] : MIN(yx[1], map->width);
+		map->width = (map->width == -1) ? yx[1] : MIN(yx[1], map->width);
+		(map->width == 0) ? error("Error: invalid file") : map->width;
 		yx[1] = -1;
 		map->file[yx[0]] = (int *)ft_memalloc(map->width * sizeof(int));
 		while (split[++yx[1]])
@@ -64,7 +64,6 @@ static int		read_file(int const fd, t_map *map)
 		free(split);
 		yx[0]++;
 	}
-	return (0);
 }
 
 /*
@@ -73,16 +72,6 @@ static int		read_file(int const fd, t_map *map)
 
 static int		count_lines(int const fd)
 {
-    /*
-    ** This code should be replaced by a lighter code.
-    ** Currently, it's very memory intensive as it uses ft_readline (GNL).
-    **
-    ** Pseudo-code:
-    ** int  count;
-    **
-    ** count = 0;
-    **
-    */
 	int		nblines;
 	char	*line;
 
@@ -98,21 +87,11 @@ static int		count_lines(int const fd)
 /*
 ** We try to open what's behind the filename. See open_file().
 ** If OK, we read the file, close the fd, and return the map.
-** TODO: Improve open_file() speed. count_lines() is fast enough.
 */
 
 int				import_from_file(char const *filename, t_map *map)
 {
 	int		fd;
-
-    struct timespec t;
-    int64_t			tt_start;
-    int64_t			tt_end;
-
-    clock_gettime(CLOCK_REALTIME, &t);
-    tt_start = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-    ft_putstr("Counting number of lines...\n");
-
 
 	if ((fd = open_file(filename)) >= 0)
 	{
@@ -121,13 +100,6 @@ int				import_from_file(char const *filename, t_map *map)
 	}
 	else
 		return (-1);
-
-    clock_gettime(CLOCK_REALTIME, &t);
-    tt_end = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-    ft_putnbr(tt_end - tt_start);
-    ft_putchar('\n');
-	ft_putstr("Done.\n");
-
 	ft_putstr("Reading file...\n");
 	if ((fd = open_file(filename)) >= 0)
 	{
