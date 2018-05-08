f22ae4b366028f505a6757b01e9d684eef01b258
William Blondel <contact@williamblondel.fr>
2018-05-08 18:27:22 +0200

    Refactoring: time function

--- src/hooks_others.c
+++ src/hooks_others.c
@@ -6,7 +6,7 @@
 /*   By: wblondel <wblondel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/20 16:32:52 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/07 10:33:33 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/05/08 18:27:02 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -34,6 +34,15 @@
 **}
 */
 
+
+int64_t		get_time(void)
+{
+	struct timespec t;
+
+	clock_gettime(CLOCK_REALTIME, &t);
+	return (t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000);
+}
+
 /*
 ** It looks like the minilibx limits the FPS to 60.
 ** Good.
@@ -41,35 +50,20 @@
 
 int		loop_hook(t_global *g)
 {
-	struct timespec t;
-
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_mlx_create_image_start = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
+	g->ts_mlx_create_image_start = get_time();
 	mlx_create_image(g->mlx, &g->image);
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_mlx_create_image_end = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-
+	g->ts_mlx_create_image_end = get_time();
 	if (g->key.tab)
 		draw_help(g);
-
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_calculate_points_start = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
+	g->ts_calculate_points_start = get_time();
 	calculate_points(&g->map, &g->cam);
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_calculate_points_end = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_draw_map_start = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
+	g->ts_calculate_points_end = get_time();
+	g->ts_draw_map_start = get_time();
 	draw_map(g->image.pixels, g->map.points, &g->map);
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_draw_map_end = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_mlx_put_image_to_window_start = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
+	g->ts_draw_map_end = get_time();
+	g->ts_mlx_put_image_to_window_start = get_time();
 	mlx_put_image_to_window(g->mlx, g->window, g->image.ptr, 0, 0);
-	clock_gettime(CLOCK_REALTIME, &t);
-	g->ts_mlx_put_image_to_window_end = t.tv_sec * INT64_C(1000) + t.tv_nsec / 1000000;
-
+	g->ts_mlx_put_image_to_window_end = get_time();
 	draw_ui(g);
 	mlx_destroy_image(g->mlx, g->image.ptr);
 	return (0);
